b6833b0b1809d91ac085fecbc79d171b3964fa18
linyongjietest <627508385@qq.com>
2021-07-06 11:21:37 +0800

    v 1.0

--- run_case.py
+++ run_case.py
@@ -9,11 +9,10 @@ from test_practise import  TestAddDemo
 (start_dir='.', pattern='test*.py'):.代表当前路径（run_case.py)，'test*.py'代表搜索所有test开头的文件
 '''
 #方法一：TestLoader()
-#suite = unittest.TestLoader().discover(start_dir='.', pattern='test*.py')
-
+# suite = unittest.TestLoader().discover(start_dir='.', pattern='test*.py')
 #方法二：TestSuite()
-suite = unittest.TestSuite()                   #用TestSuite调用类TestAddDemo中的测试用例
-suite.addTest(TestAddDemo('test_add01'))
-
+# suite = unittest.TestSuite()                   #用TestSuite调用类TestAddDemo中的测试用例
+# suite.addTest(TestAddDemo('test_add01'))
+#
 runner = unittest.TextTestRunner()            #TextTestRunner:一个基础的测试用例执行器，实现了将结果输出为流的功能。如果stream为None，默认的，sys.stderr会被作为输出流。
 runner.run(suite)
